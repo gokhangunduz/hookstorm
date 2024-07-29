ae0473daf338402d4f948071bf82dabdc76b03b7
gokhangunduz <me@gokhangunduz.dev>
2024-07-30 00:12:28 +0300

    refactor(useToggle): :tada: update useToggle

--- src/useToggle.tsx
+++ src/useToggle.tsx
@@ -5,6 +5,21 @@ import { useState } from "react";
  *
  * @param initialValue - The initial state of the toggle, default is `false`.
  * @returns An object containing the current value and a function to toggle it.
+ *
+ * @example
+ * // Usage in a React component
+ * const MyComponent: React.FC = () => {
+ *   const { value, handleToggle } = useToggle();
+ *
+ *   return (
+ *     <div>
+ *       <p>Current value: {value ? "On" : "Off"}</p>
+ *       <button onClick={() => handleToggle()}>Toggle</button>
+ *       <button onClick={() => handleToggle(true)}>Set On</button>
+ *       <button onClick={() => handleToggle(false)}>Set Off</button>
+ *     </div>
+ *   );
+ * };
  */
 const useToggle = (initialValue: boolean = false) => {
   // State to hold the current value of the toggle
