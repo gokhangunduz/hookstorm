87998347bda1ae2327c44efc3d06b18ccd4530ca
gokhangunduz <me@gokhangunduz.dev>
2024-07-30 00:04:51 +0300

    refactor(useToggle): :tada: update useToggle

--- src/useToggle.tsx
+++ src/useToggle.tsx
@@ -1,18 +1,28 @@
 import { useState } from "react";
 
-const useToggle = (defaultValue?: boolean) => {
-  const [value, setToggle] = useState<boolean>(defaultValue || false);
+/**
+ * Custom hook to toggle a boolean value.
+ *
+ * @param initialValue - The initial state of the toggle, default is `false`.
+ * @returns An object containing the current value and a function to toggle it.
+ */
+const useToggle = (initialValue: boolean = false) => {
+  // State to hold the current value of the toggle
+  const [value, setToggle] = useState<boolean>(initialValue);
 
-  function handleToggle(value?: boolean) {
-    setToggle(value || !value);
+  /**
+   * Function to toggle the value.
+   *
+   * @param newValue - If provided, sets the toggle to this value. Otherwise, toggles the current value.
+   */
+  function handleToggle(newValue?: boolean): void {
+    setToggle((prev) => newValue ?? !prev);
   }
 
-  const useToggle = {
+  return {
     value,
     handleToggle,
   };
-
-  return useToggle;
 };
 
 export default useToggle;
